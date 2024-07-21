cadf5ede8546f1d729de8c59acd20ba15d78fd43
salamdunya1 <burmurdack@gmail.com>
2024-07-21 18:48:57 +0000

    hello

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,12 +1,10 @@
 export default function createReportObject(employeesList) {
-    const obj = {
-        allEmployees: {...employeesList},
-        getNumberOfDepartments(employeesList){
-            let idx = 0
-            for (let employee in employeesList)
-                idx++
-            return idx
-        } 
-    }
-    return obj
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
 }
