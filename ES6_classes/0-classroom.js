639f26fd06f591712eb5cc6594b9e6d93c4c3500
salamdunya1 <burmurdack@gmail.com>
2024-07-21 18:13:16 +0000

    hello

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,6 @@
+/* eslint-disable */
 export default class ClassRoom {
-  constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
-  }
+	constructor(maxStudentsSize) {
+		this._maxStudentsSize = maxStudentsSize;
+	}
 }
