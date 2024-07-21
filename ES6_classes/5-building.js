95dc893a75bac3205daeccff74549f87a4729dbf
salamdunya1 <burmurdack@gmail.com>
2024-07-21 18:05:47 +0000

    hello world

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+// 5-building.js
+
+class Building {
+    constructor(sqft) {
+      if (
+        this.constructor !== Building
+          && typeof this.evacuationWarningMessage !== 'function'
+      ) {
+        throw Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+  
+      this._sqft = sqft;
+    }
+  
+    // Getter for sqft
+    get sqft() {
+      return this._sqft;
+    }
+  }
+  
+  export default Building;
+  
