4ad251a9ce3463d42746185577659a0f5d9fee8d
salamdunya1 <burmurdack@gmail.com>
2024-07-21 18:11:47 +0000

    hello

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,23 +1,12 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-      this._size = size;
-      this._location = location;
-    }
-  
-    get size() {
-      return this._size;
-    }
-  
-    get location() {
-      return this._location;
-    }
-  
-    valueOf() {
-      return this._size;
-    }
-  
-    toString() {
-      return this._location;
-    }
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
   }
-  
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') return this._location;
+    if (hint === 'number') return this._size;
+    return 42;
+  }
+}
