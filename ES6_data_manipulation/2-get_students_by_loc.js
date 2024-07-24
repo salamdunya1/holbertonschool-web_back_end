988a5f138c9c473ad61e1168acd46f4418422dee
salamdunya1 <burmurdack@gmail.com>
2024-07-24 09:18:02 +0000

    x

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+export default function getStudentsByLocation(studentsArray, city) {
+  const selected = studentsArray.filter((student) => student.location === city);
+  return selected;
+}
